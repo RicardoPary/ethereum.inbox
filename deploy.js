983ee240f591a950cea453ae9012a904a9ca1b65
Ricardo Pari <rikiRicardo@hotmail.com>
2021-09-29 20:24:36 -0400

    order code

--- deploy.js
+++ deploy.js
@@ -1,10 +1,13 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const {interface, bytecode} = require('./compile')
+const {environment} = require("./environment");
+
+const INITIAL_STRING = 'Hi there!'
 
 const provider = new HDWalletProvider(
-    '',
-    'https://rinkeby.infura.io/v3/cdb1db7fb3e044f7b251855b1083804d'
+    environment.mnemonic,
+    environment.network
 );
 
 const web3 = new Web3(provider);
@@ -12,13 +15,13 @@ const web3 = new Web3(provider);
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy from account', accounts[0])
+    console.log('Attempting to deploy from account: ', accounts[0])
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!']})
-        .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+        .deploy({data: bytecode, arguments: [INITIAL_STRING]})
+        .send({gas: '1000000', gasPrice: '5000000000', from: accounts[0]});
 
-    console.log('Contract deployed to', result.options.address);
+    console.log('Contract deployed to: ', result.options.address);
 
 };
 
